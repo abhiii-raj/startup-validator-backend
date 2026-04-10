faca8a8f2f96ff7809ca516575d1f799c25bfacc
Abhi_Raj <abhishubhraj111@gmail.com>
2026-04-10 13:25:46 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 const { app } = require("./app");
-const { env } = require("./config/env");
 const { connectMongo, connectPostgresIfEnabled } = require("./config/db");
 
-async function start() {
+// Initialize database connections
+(async () => {
   await connectMongo();
   await connectPostgresIfEnabled();
+})();
 
-  app.listen(env.port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`API running on http://localhost:${env.port}`);
-  });
-}
-
-start().catch((error) => {
-  // eslint-disable-next-line no-console
-  console.error("Failed to start server:", error);
-  process.exit(1);
-});
+// Export for Vercel serverless
+module.exports = app;
